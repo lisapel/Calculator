7eb41faa785b77ace5229b8522dc55b2218358fc
lisapel <lisa.peldan@gmail.com>
2023-02-25 13:33:21 +0100

    reformat

--- src/Mathematical_Op.java
+++ src/Mathematical_Op.java
@@ -1,36 +1,37 @@
 
 
-public class Mathematical_Op  {
+public class Mathematical_Op {
 
     final Operands addition = Double::sum;
-    final Operands subtraction = (a,b) ->a-b;
-    final Operands division = (a,b) -> a/b;
-    final Operands modulus = (a,b) ->a%b;
-    final Operands multiplication = (a,b) -> a*b;
+    final Operands subtraction = (a, b) -> a - b;
+    final Operands division = (a, b) -> a / b;
+    final Operands modulus = (a, b) -> a % b;
+    final Operands multiplication = (a, b) -> a * b;
 
 
-
-    protected double evaluate(double a, double b, Tokens token){
-        switch (token){
+    protected double evaluate(double a, double b, Tokens token) {
+        switch (token) {
             case add -> {
-               return addition.operate(a,b);
+                return addition.operate(a, b);
             }
             case div -> {
-                return division.operate(a,b);
+                return division.operate(a, b);
             }
             case mod -> {
-                return modulus.operate(a,b);
+                return modulus.operate(a, b);
             }
             case mul -> {
-                return multiplication.operate(a,b);
+                return multiplication.operate(a, b);
             }
             case sub -> {
-                return subtraction.operate(a,b);
+                return subtraction.operate(a, b);
             }
             default -> {
                 return 0.0;
             }
-        }}}
+        }
+    }
+}
 
 
 
